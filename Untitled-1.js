05a232ebef21f76054d2108d64cb78dfc6c8ce56
carthyns <carthyns@163.com>
2018-01-30 21:08:06 +0800

    first test

---
+++ Untitled-1.js
@@ -0,0 +1,15 @@
+var obj = {
+    nickname: '小明',
+    is_vip: true
+}
+
+nickname='小明';
+
+var obj2 = {
+    vipname:'Lily',
+    vipCode:'123456'
+}
+
+document.cookie="vipname=Lily;vipCode=123456;";
+
+"vipname=Lily&vipCode=12345&vipl=1234&vipl=56"//query固定格式；
